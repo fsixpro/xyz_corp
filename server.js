902a0813b31af64c3cebfc8533c0da5c10e54266
Francis Ayodeji <fsixpro@gmail.com>
2020-12-10 04:49:18 +0100

    prepare to deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import path from 'path'
-import morgan from 'morgan'
+
 import userRoute from './routes/userRoute.js'
 import channelRoute from './routes/channelRoute.js'
 import db from './Db.js'
@@ -12,7 +12,6 @@ dotenv.config()
 db()
 app.use(cors())
 app.use(express.json())
-app.use(morgan)
 app.use('/api/user', userRoute)
 app.use('/api/channel', channelRoute)
 
@@ -25,6 +24,6 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
