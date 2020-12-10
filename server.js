ad9000dc85e452d58c691ec95b3f01998b9e1e94
Francis Ayodeji <fsixpro@gmail.com>
2020-12-10 04:17:44 +0100

    prepare to deploy

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import path from 'path'
+import morgan from 'morgan'
 import userRoute from './routes/userRoute.js'
 import channelRoute from './routes/channelRoute.js'
 import db from './Db.js'
@@ -10,9 +12,19 @@ dotenv.config()
 db()
 app.use(cors())
 app.use(express.json())
-
+app.use(morgan)
 app.use('/api/user', userRoute)
 app.use('/api/channel', channelRoute)
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 const PORT = process.env.PORT
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
